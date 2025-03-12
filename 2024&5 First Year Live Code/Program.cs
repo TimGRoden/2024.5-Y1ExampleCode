b4d9848dd1d60d6f3cfc1889cfb029844435e122
Timothy Roden <TGR@barton.ac.uk>
2025-03-12 09:29:54 +0000

    Add project files.

---
+++ 2024&5 First Year Live Code/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024_5_First_Year_Live_Code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
